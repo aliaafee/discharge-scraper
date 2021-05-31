9bc77f3d8d0450ff30497e1666a1633218a4d298
Ali Aafee <ali.aafee@gmail.com>
2021-05-31 20:03:26 +0545

    Fixed some stuff

--- extract_log_book.py
+++ extract_log_book.py
@@ -6,42 +6,38 @@ scraper = Scraper()
 
 
 #Fields that will be extracted
-FIELDS = collections.OrderedDict()
-FIELDS["name"] = ["Name:"]
-FIELDS["age"] = ["Age:"]
-FIELDS["sex"] = ["Sex:"]
-FIELDS["address"] = ["Address:"]
-FIELDS["ip_number"] = ["IP No.:", "Inpatient no:"]
-FIELDS["phone"] = ["Contact No:", "Contact no.:", "Contact:"]
-#FIELDS["admitted_bs"] = ["Date of Admission:"]
-#FIELDS["discharged_bs"] = ["Date of Discharge:", "Date of Transfer:"]
-FIELDS["admitted_ad"] = ["Date of Admission:"]
-FIELDS["discharged_ad"] = ["Date of Discharge:", "Date of Transfer:"]
-FIELDS["diagnosis"] = ["Final Diagnosis", "FINAL DIAGNOSIS:"]
-#FIELDS["operated_bs"] = ["Date:", "Operations", "Surgery:"]
-FIELDS["operated_ad"] = ["Date:", "Operations", "Surgery:"]
-FIELDS["surgeons"] = ["Surgeons:", "Surgeon:", "Surgeons :"]
-FIELDS["operation"] = ["Operations", "Surgery:"]
+fields = collections.OrderedDict()
+fields["name"] = ["Name:"]
+fields["age"] = ["Age:"]
+fields["sex"] = ["Sex:"]
+fields["address"] = ["Address:"]
+fields["ip_number"] = ["IP No.:", "Inpatient no:"]
+fields["phone"] = ["Contact No:", "Contact no.:", "Contact:"]
+#fields["admitted_bs"] = ["Date of Admission:"]
+#fields["discharged_bs"] = ["Date of Discharge:", "Date of Transfer:"]
+fields["admitted_ad"] = ["Date of Admission:"]
+fields["discharged_ad"] = ["Date of Discharge:", "Date of Transfer:"]
+fields["diagnosis"] = ["Final Diagnosis", "FINAL DIAGNOSIS:"]
+#fields["operated_bs"] = ["Date:", "Operations", "Surgery:"]
+fields["operated_ad"] = ["Date:", "Operations", "Surgery:"]
+fields["surgeons"] = ["Surgeons:", "Surgeon:", "Surgeons :"]
+fields["operation"] = ["Operations", "Surgery:"]
 
-scraper.set_field_definition(FIELDS)
-
-
-#set custom field process
-FIELD_PROCESS = {}
-FIELD_PROCESS["age"] = scraper.default_process + [scraper.extract_age]
-FIELD_PROCESS["sex"] = scraper.default_process + [scraper.sanitize_sex]
-FIELD_PROCESS["admitted_bs"] = scraper.default_process + [scraper.extract_date, scraper.sanitize_date]
-FIELD_PROCESS["discharged_bs"] = scraper.default_process + [scraper.extract_date, scraper.sanitize_date]
-FIELD_PROCESS["admitted_ad"] = FIELD_PROCESS["admitted_bs"] + [scraper.convert_date_bs2ad]
-FIELD_PROCESS["discharged_ad"] = FIELD_PROCESS["discharged_bs"] + [scraper.convert_date_bs2ad]
-FIELD_PROCESS["surgeons"] = scraper.default_process
-FIELD_PROCESS["operation"] = scraper.default_process + [scraper.remove_date]
-FIELD_PROCESS["operated_bs"] = scraper.default_process + [scraper.extract_date, scraper.sanitize_date]
-FIELD_PROCESS["operated_ad"] = FIELD_PROCESS["discharged_bs"] + [scraper.convert_date_bs2ad]
-
-scraper.set_custom_processing(FIELD_PROCESS)
+#Custom field process
+field_process = {}
+field_process["age"] = scraper.default_process + [scraper.extract_age]
+field_process["sex"] = scraper.default_process + [scraper.sanitize_sex]
+field_process["admitted_bs"] = scraper.default_process + [scraper.extract_date, scraper.sanitize_date]
+field_process["discharged_bs"] = scraper.default_process + [scraper.extract_date, scraper.sanitize_date]
+field_process["admitted_ad"] = field_process["admitted_bs"] + [scraper.convert_date_bs2ad]
+field_process["discharged_ad"] = field_process["discharged_bs"] + [scraper.convert_date_bs2ad]
+field_process["surgeons"] = scraper.default_process
+field_process["operation"] = scraper.default_process + [scraper.remove_date]
+field_process["operated_bs"] = scraper.default_process + [scraper.extract_date, scraper.sanitize_date]
+field_process["operated_ad"] = field_process["discharged_bs"] + [scraper.convert_date_bs2ad]
 
 
-#Extract Data to CSV
+scraper.set_field_definition(fields)
+scraper.set_custom_processing(field_process)
 with CSVWriter('test.csv', scraper.get_field_names()) as writer:
     scraper.extract_data("./test", writer)
